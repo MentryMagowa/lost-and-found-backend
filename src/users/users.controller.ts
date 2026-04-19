e0a6d26ff9f085db2f8302621e6319e6cf3f17b5
efp-bsc-com-dotcm <efp-bsc-com@unima.ac.mw>
2026-04-19 14:35:40 +0200

    adde stuff

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './dto/create-dto';
+import {UsersService} from "./users.service";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.usersService.findAll();
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+        // Note: +id converts the string param to a number
+        return this.usersService.update(+id, updateUserDto);
+    }
+
+    // ... other methods call this.usersService.xxx
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(+id); // Method is now "used"
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.usersService.remove(+id); // Method is now "used"
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './dto/create-dto';
+
+// Define the shape of your User object
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    age: number;
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    // Fix: Explicitly type the array as User[] instead of letting it be 'never[]'
+    private users: User[] = [];
+
+    create(createUserDto: CreateUserDto): User {
+        const newUser: User = {
+            id: Date.now(),
+            ...createUserDto
+        };
+        this.users.push(newUser);
+        return newUser;
+    }
+
+    findAll(): User[] {
+        return this.users;
+    }
+
+    // To fix the "Unused method" errors, ensure these are called in your Controller!
+    findOne(id: number): User {
+        const user = this.users.find(u => u.id === id);
+        if (!user) throw new NotFoundException(`User with ID ${id} not found`);
+        return user;
+    }
+
+    update(id: number, updateUserDto: UpdateUserDto): User {
+        const userIndex = this.users.findIndex(u => u.id === id);
+        if (userIndex === -1) throw new NotFoundException(`User with ID ${id} not found`);
+
+        this.users[userIndex] = { ...this.users[userIndex], ...updateUserDto };
+        return this.users[userIndex];
+    }
+
+    remove(id: number) {
+        const userIndex = this.users.findIndex(u => u.id === id);
+        if (userIndex === -1) throw new NotFoundException(`User with ID ${id} not found`);
+
+        return this.users.splice(userIndex, 1)[0];
+    }
+}
