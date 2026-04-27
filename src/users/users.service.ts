0f33c9ac38e0eb3eacc03637c8be79606532b755
efp-bsc-com-01-24-dot-com <efp-bsc-com-01-24@unima.ac.mw>
2026-04-27 21:32:07 +0200

    Refactor create and remove methods in UsersService
    Update create and remove methods to return messages along with user data.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,14 +14,19 @@ export class UsersService {
     // Fix: Explicitly type the array as User[] instead of letting it be 'never[]'
     private users: User[] = [];
 
-    create(createUserDto: CreateUserDto): User {
-        const newUser: User = {
-            id: Date.now(),
-            ...createUserDto
-        };
-        this.users.push(newUser);
-        return newUser;
-    }
+  create(createUserDto: CreateUserDto) {
+    const newUser: User = {
+        id: Date.now(),
+        ...createUserDto
+    };
+
+    this.users.push(newUser);
+
+    return {
+        message: 'User created',
+        user: newUser
+    };
+}
 
     findAll(): User[] {
         return this.users;
@@ -43,9 +48,16 @@ export class UsersService {
     }
 
     remove(id: number) {
-        const userIndex = this.users.findIndex(u => u.id === id);
-        if (userIndex === -1) throw new NotFoundException(`User with ID ${id} not found`);
-
-        return this.users.splice(userIndex, 1)[0];
+    const userIndex = this.users.findIndex(u => u.id === id);
+    if (userIndex === -1) {
+        throw new NotFoundException(`User with ID ${id} not found`);
     }
+
+    const deletedUser = this.users.splice(userIndex, 1)[0];
+
+    return {
+        message: 'User deleted',
+        user: deletedUser
+    };
+}
 }
