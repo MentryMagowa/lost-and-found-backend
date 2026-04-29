fad54728d68a202d69273e54b68717d2755217a4
bed-com-38-22 <bed-com-38-22@unima.ac.mw>
2026-04-29 04:34:43 -0800

    removing the nested folder

--- lost-and-found-backend/src/found-items/dto/update-found-item.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFoundItemDto } from './create-found-item.dto';
-
-export class UpdateFoundItemDto extends PartialType(CreateFoundItemDto) {}
